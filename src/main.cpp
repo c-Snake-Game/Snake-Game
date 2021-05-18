1d76f760dd1b679b57dcf60138286e344f273e3c
kmuHjh <gkwjdgus42@gmail.com>
2021-05-18 20:28:41 +0900

    add base file

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <ncurses.h>
+
+int main(int argc, char *argv[]) {
+  initscr();
+  start_color();
+  init_pair(1, COLOR_RED, COLOR_WHITE);
+  box(stdscr, 0, 0);
+  attron(COLOR_PAIR(1));
+  wbkgd(stdscr, COLOR_PAIR(1));  //윈도우 백그라운드 컬러
+  // 기본 윈도우 내의 서브윈도우 생성
+  WINDOW *win = subwin(stdscr, 5, 60, 10, 10);
+  init_pair(2, COLOR_BLACK, COLOR_BLUE);  //윈도우 백그라운드 컬러
+  box(win, 0, 0);
+  attron(COLOR_PAIR(2));
+  wbkgd(win, COLOR_PAIR(2));
+  refresh();
+  getch();
+  delwin(win);
+  endwin();
+  return 0;
+}
---
+++ src/pack.cpp
@@ -0,0 +1,5 @@
+#include "pack.hpp"
+
+#include <iostream>
+
+void PrintOut(const std::string &message) { std::cout << message << std::endl; }
