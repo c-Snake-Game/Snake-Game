67dddad5178682980783eb7df61a9779a133ed99
kmuHjh <66051301+kmuHjh@users.noreply.github.com>
2021-06-01 20:26:58 +0900

    Update snake.h

--- src/snake.h
+++ src/snake.h
@@ -8,33 +8,27 @@
 #include <termios.h>   //kgetch
 #include <term.h>         //kgetch
 
-
 #ifndef SNAKE_H
 #define SNAKE_H
-int kbhit();
+
+int kbhit();     //key board hit 
 
 class Board{
-public:
-    Board();
-    ~Board();
-    int boardList[32][64];
-    void printBoard();
+    public:
+        Board();
+        ~Board();
+        int boardList[32][64];
+        void printBoard();
 };
 
 class Snake{
-private:
-    int x;
-    int y;
-public:
-    Snake(int headx, int heady){
-        x = headx;
-        y = heady;
-    }
-    int getX(){return x;}
-    int getY(){return y;}
-    
+    private:
+        int x;
+        int y;
+    public:
+        Snake(int headx, int heady):x(headx), y(heady){}
+        int getX(){return x;}
+        int getY(){return y;}
 };
-
-
 
 #endif
